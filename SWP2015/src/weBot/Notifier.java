d669b6ac52f70430269bc55179f5bc4af173f376
Andreas Knapp <andreas.knapp.135@gmail.com>
2015-06-02 13:44:19 +0200

    Update Comments on Notifier

--- SWP2015/src/weBot/Notifier.java
+++ SWP2015/src/weBot/Notifier.java
@@ -3,12 +3,15 @@ package weBot;
 public interface Notifier {
 
 	/**
-	 * Sended @message an @address (Phone, Mail...)
+	 * Sended @message
+	 * 
+	 * Konstruktor für jeweiligen Notifier bekommt als Übergabeparamer die
+	 * Adresse, Phone-Nummer o.ä.
 	 * 
 	 * @param address
 	 * @param message
 	 * 
-	 * @return String 
+	 * @return String
 	 */
 	public String notify(String subject, String message);
 
