50105bd9fdd82401d913e14380850a0b81c645df
Andreas Knapp <andreas.knapp.135@gmail.com>
2015-06-26 09:09:30 +0200

    Fixes minimal Bugs, added LogTable

--- SWP2015/src/gui/GUI.java
+++ SWP2015/src/gui/GUI.java
@@ -2,28 +2,41 @@ package gui;
 
 import java.io.File;
 import java.util.List;
-
+import weBot.Log;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class GUI extends Application {
 
 	private List<Object> instructionList = null;
 	private File logicFile = null;
-
 	boolean browserChosen = false;
 	boolean logicAdded = false;
+
+	private final Label browserLabel = new Label("Choose your Browser:");
+	private ComboBox<String> browserBox = new ComboBox<String>();
+	private TextField browserPath = new TextField();
+	private Button chooseBrowser = new Button("Choose Browser");
+	private Button discardBrowser = new Button("Discard Browser");
+	private Label chosenBrowser = new Label("Chosen Browser: none");
 
 	private final Label chooseLogic = new Label("Choose your Logic:");
 	private final Label chosenLogic = new Label("Chosen Logic-File: none");
@@ -32,13 +45,13 @@ public class GUI extends Application {
 
 	private final Button startButton = new Button("Start");
 	private final Button stopButton = new Button("Stop");
-
-	private final Label browserLabel = new Label("Choose your Browser:");
-	private ComboBox<String> browserBox = new ComboBox<String>();
-	private TextField browserPath = new TextField();
-	private Button chooseBrowser = new Button("Choose Browser");
-	private Button discardBrowser = new Button("Discard Browser");
-	private Label chosenBrowser = new Label("Chosen Browser: none");
+	
+	private Label scoreLabel = new Label("No Score");
+	private Label scoreText = new Label("Nothing to Watch");
+	
+	private TableView<Log> logTable = new TableView<Log>();
+	private TableColumn<Log, String> logColumn = new TableColumn<Log, String>();
+	private ObservableList<Log> logList = FXCollections.observableArrayList(new Log("Programm Started"));
 
 	private final Label statusLabel = new Label("Status:");
 	private Label statusText = new Label("Waiting for Operator...");
@@ -81,6 +94,8 @@ public class GUI extends Application {
 		chooseBrowser.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(final ActionEvent e) {
+				Log log = new Log("Test");
+				addLog(log);
 				if (browserBox.getValue().equals("FireFox")) {
 					browserPath.setDisable(true);
 					chooseBrowser.setDisable(true);
@@ -126,11 +141,13 @@ public class GUI extends Application {
 				discardBrowser.setDisable(true);
 				browserBox.setDisable(false);
 				chooseBrowser.setDisable(false);
-				browserPath.setDisable(false);
+				if (!browserBox.getValue().equals("FireFox"))
+					browserPath.setDisable(false);
 				browserChosen = false;
 				if (logicAdded)
 					statusText.setText("Waiting for Browser");
-				else statusText.setText("Waiting for Inputs");
+				else
+					statusText.setText("Waiting for Inputs");
 			}
 		});
 
@@ -147,7 +164,7 @@ public class GUI extends Application {
 			public void handle(final ActionEvent e) {
 				FileChooser fileChooser = new FileChooser();
 				ExtensionFilter filter = new ExtensionFilter("BotLanguage (*.bla)", "*.bla");
-				
+
 				fileChooser.getExtensionFilters().add(filter);
 				logicFile = fileChooser.showOpenDialog(arg0);
 				if (logicFile != null && logicFile.getName().endsWith(".bla")) {
@@ -229,15 +246,33 @@ public class GUI extends Application {
 			public void handle(ActionEvent arg0) {
 				// TODO
 				// Stoppt das Programm
-				discardLogic.setDisable(false);
-				discardBrowser.setDisable(false);
-				chooseBrowser.setDisable(true);
-				stopButton.setDisable(true);
-				startButton.setDisable(false);
-				statusText.setText("Stopped by Operator");
+				stop("Stopped by Operator!");
 
 			}
 		});
+		
+		scoreLabel.setLayoutX(10);
+		scoreLabel.setLayoutY(250);
+		scoreText.setLayoutX(10);
+		scoreText.setLayoutY(270);
+		
+		logTable.setLayoutX(10);
+		logTable.setLayoutY(300);
+		logTable.setMaxHeight(150);
+		logTable.setMaxWidth(570);
+		logTable.setMinWidth(570);
+		logColumn.setMinWidth(568);
+		logColumn.setMaxWidth(568);
+		logColumn.setText("Logs");
+		logColumn.setCellValueFactory(new Callback<CellDataFeatures<Log, String>, ObservableValue<String>>() {
+			public ObservableValue<String> call(CellDataFeatures<Log, String> p) {
+				return p.getValue().getLogProperty();
+			}
+		});	
+		
+
+		logTable.setItems(logList);
+		logTable.getColumns().add(logColumn);	
 
 		statusLabel.setLayoutX(10);
 		statusLabel.setLayoutY(500);
@@ -245,7 +280,9 @@ public class GUI extends Application {
 		statusText.setLayoutY(500);
 
 		pane.getChildren().addAll(chooseLogic, loadLogic, discardLogic, chosenLogic, browserBox, browserLabel, browserPath, chooseBrowser, discardBrowser, chosenBrowser,
-				startButton, stopButton, statusLabel, statusText);
+				startButton, stopButton, scoreLabel, scoreText, logTable, statusLabel, statusText);
+		
+		
 
 		Scene scene = new Scene(pane);
 		Stage stage = new Stage();
@@ -270,5 +307,24 @@ public class GUI extends Application {
 	public void setStatus(String message) {
 		statusText.setText(message);
 	}
+
+	public void stop(String message) {
+		discardLogic.setDisable(false);
+		discardBrowser.setDisable(false);
+		chooseBrowser.setDisable(true);
+		stopButton.setDisable(true);
+		startButton.setDisable(false);
+		statusText.setText(message);
+	}
+	
+	public void setScore(String name, String message){
+		scoreLabel.setText(name + ":");
+		scoreText.setText(message);
+	}
+	
+	public void addLog(Log log){
+		logList.add(0, log);
+	}
+		
 
 }
